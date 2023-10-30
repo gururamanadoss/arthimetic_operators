0de74df68ab1423bf2bd7bdd1c0d07c03502b2c8
gururamanadoss <147541827+gururamanadoss@users.noreply.github.com>
2023-10-30 12:37:54 +0530

    Update arithematic_operators.py

--- arithematic_operators.py
+++ arithematic_operators.py
@@ -1,6 +1,11 @@
 print("arthematic operators")
-num1=20
-num2=30
+num1=12
+num2=36
 print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)             
 print("num1,num2 operands")
 print(20,30 "operators")
